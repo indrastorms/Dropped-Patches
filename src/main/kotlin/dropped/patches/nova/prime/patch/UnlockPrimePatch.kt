e74b0d05320ef055e1f58fea17f81b1eeeb50842
Indranil012 <indranilchowdhury0@gmail.com>
2024-02-11 16:35:05 +0530

    fix build

--- src/main/kotlin/dropped/patches/nova/prime/patch/UnlockPrimePatch.kt
+++ src/main/kotlin/dropped/patches/nova/prime/patch/UnlockPrimePatch.kt
@@ -5,7 +5,8 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patches.nova.prime.fingerprints.UnlockPrimeFingerprint
 import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction11x
 
