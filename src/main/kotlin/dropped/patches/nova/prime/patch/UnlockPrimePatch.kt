4780e3ccc4cc78e9bd080814d192ce0ae33a7a26
Indranil012 <indranilchowdhury0@gmail.com>
2024-02-11 17:25:21 +0530

    fix build

--- src/main/kotlin/dropped/patches/nova/prime/patch/UnlockPrimePatch.kt
+++ src/main/kotlin/dropped/patches/nova/prime/patch/UnlockPrimePatch.kt
@@ -1,6 +1,6 @@
 package app.revanced.patches.nova.prime.patch
 
-import app.revanced.patcher.patch.PatchException
+import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
@@ -15,32 +15,26 @@ import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction11x
     description = "Unlocks Nova Prime and all functions of the app.",
     compatiblePackages = [CompatiblePackage("com.teslacoilsw.launcher")]
 )
-class UnlockPrimePatch : BytecodePatch(
-    setOf(
-        UnlockPrimeFingerprint
-    )
+object UnlockPrimePatch : BytecodePatch(
+    setOf(UnlockPrimeFingerprint)
 ) {
-    private companion object {
+    override fun execute(context: BytecodeContext) {
+        UnlockPrimeFingerprint.result?.apply {
         // Any value except 0 unlocks prime, but 512 is needed for a protection mechanism
         // which would reset the preferences if the value on disk had changed after a restart.
         const val PRIME_STATUS: Int = 512
-    }
+        val insertIndex = scanResult.patternScanResult!!.endIndex + 1
 
-    override fun execute(context: BytecodeContext): PatchResult {
-        UnlockPrimeFingerprint.result?.apply {
-            val insertIndex = scanResult.patternScanResult!!.endIndex + 1
+        val primeStatusRegister =
+            (mutableMethod.implementation!!.instructions[insertIndex - 1] as BuilderInstruction11x).registerA
 
-            val primeStatusRegister =
-                (mutableMethod.implementation!!.instructions[insertIndex - 1] as BuilderInstruction11x).registerA
+        mutableMethod.addInstruction(
+            insertIndex,
+            """
+                const/16 v$primeStatusRegister, $PRIME_STATUS
+            """
+        )
+    } ?: throw UnlockPrimeFingerprint.exception
 
-            mutableMethod.addInstruction(
-                insertIndex,
-                """
-                    const/16 v$primeStatusRegister, $PRIME_STATUS
-                """
-            )
-        } ?: return UnlockPrimeFingerprint.PatchException()
-
-        return PatchResult()
-    }
+  }
 }
