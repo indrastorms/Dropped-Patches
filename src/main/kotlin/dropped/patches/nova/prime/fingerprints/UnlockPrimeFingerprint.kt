5e2884b35b63c968a687078efe4da571561bf5c4
oSumAtrIX <github@osumatrix.me>
2024-02-18 20:53:53 +0530

    fix(nova): Update patch

--- src/main/kotlin/dropped/patches/nova/prime/fingerprints/UnlockPrimeFingerprint.kt
+++ src/main/kotlin/dropped/patches/nova/prime/fingerprints/UnlockPrimeFingerprint.kt
@@ -3,14 +3,7 @@ package app.revanced.patches.nova.prime.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.Opcode
 
-object UnlockPrimeFingerprint : MethodFingerprint(
-    "V",
-    opcodes = listOf(
-        Opcode.IPUT_OBJECT,
-        Opcode.CONST_STRING,
-        Opcode.CONST_4,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT
-    ),
-    strings = listOf("1")
+object UpdatePrimeStatusFingerprint : MethodFingerprint(
+    opcodes = listOf(Opcode.CONST_4), // Modify this to change the prime status.
+    strings = listOf("widget_reset_ids")
 )
