01133f091168aa816f3f9e6291bda658ebf9fdcb
Indranil012 <indranilchowdhury0@gmail.com>
2024-07-25 01:34:02 +0530

    refactor: build to dsl

---
+++ patches/build.gradle.kts
@@ -0,0 +1,16 @@
+group = "dropped.patches"
+
+patches {
+    about {
+        Name = "Dropped Patches"
+        Description = "Patches for ReVanced."
+        Version = version
+        Timestamp = System.currentTimeMillis().toString()
+        Source = "git@github.com:you/revanced-patches.git"
+        Author = "indrastorms"
+        Contact = "https://t.me/Indra012"
+        Origin = "https://github.com/indrastorms/Dropped-Patches"
+        License = "GNU General Public License v3.0"
+    }
+}
+  
