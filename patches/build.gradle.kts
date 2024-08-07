60054eeca163816610e193d80d077077f3e73c30
Indranil012 <indranilchowdhury0@gmail.com>
2024-08-07 22:04:33 +0530

    fix: upstream build

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -10,3 +10,38 @@ patches {
         license = "GNU General Public License v3.0"
     }
 }
+dependencies {
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+    // Required due to smali, or build fails. Can be removed once smali is bumped.
+    implementation(libs.guava)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesFiles") {
+        description = "Generate patches files"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.revanced.generator.MainKt")
+    }
+
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesFiles")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/indrastorms/dropped-patches")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
