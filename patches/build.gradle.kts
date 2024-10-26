9135f47b290ed6812fac41f407c29bfa497493c1
Indra <48489074+indrastorms@users.noreply.github.com>
2024-10-26 23:59:16 +0530

    refactor(dsl): upgrage patcher and refactor patches to use new apis (#66)
    * refactor: build
    
    * refactor: build to dsl
    
    * refactor: more files migrated
    
    * refactor: more files migrated
    
    * refactor: complete migration to dsl
    
    * fix: Bump gradle version
    
    * refactor: format code
    
    * fix: remove kls database file
    
    * fix: add kls file to gitignore
    
    * fix: upstream build
    
    * build: upgrade wrapper
    
    * fix
    
    * remove unnessary tasks
    
    * fix: gen bundle script
    
    * refactor: cleanup workflows
    
    * build: bump deps

---
+++ patches/build.gradle.kts
@@ -0,0 +1,12 @@
+group = "dropped.patches"
+
+patches {
+    about {
+        name = "Dropped Patches"
+        description = "Patches for ReVanced."
+        source = "git@github.com:you/revanced-patches.git"
+        author = "indrastorms"
+        contact = "https://t.me/Indra012"
+        license = "GNU General Public License v3.0"
+    }
+}
